34f4de3ae332c3193e3e5358b48bca7b6de5f3a4
Shogo Sonoda <sonoshou@gmail.com>
2015-12-08 15:54:16 +0900

    Reloaderとhashのサンプルを追加

---
+++ hash.rb
@@ -0,0 +1,3 @@
+class Hash
+  List = { :test => 'test', :test2 => 'test2' }
+end
--- my_app.rb
+++ my_app.rb
@@ -1,8 +1,14 @@
 require 'bundler'
 require 'sinatra/base'
+require 'sinatra/reloader'
+require './hash'
 Bundler.require
 
 class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/' do
     @title = "Title Test"
     haml :index
@@ -10,6 +16,11 @@ class MyApp < Sinatra::Base
 
   get '/:name' do |name|
     @title = "#{name}"
+    haml :index
+  end
+
+  get '/hash' do
+    @title = Hash::List[:test]
     haml :index
   end
 end
